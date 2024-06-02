dd04474522b789eb9c220b4b3b5a27ee81449ba8
Artem Yurkov <yurkov.artyom21@gmail.com>
2024-06-02 18:49:39 +0300

    Fix caching

--- photoflow/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post, Like, Comment, Follow, Message
-
-admin.site.register(Post)
-admin.site.register(Like)
-admin.site.register(Comment)
-admin.site.register(Follow)
-admin.site.register(Message)
-
